67670c0049027e302dd317e8d990964769c3011e
Freya Fleckenstein <fleckenf@informatik.uni-freiburg.de>
2018-03-21 15:39:51 +0100

    compilable

--- include/grid_world_conversion/grid_world_conversion.h
+++ include/grid_world_conversion/grid_world_conversion.h
@@ -1,35 +1,54 @@
 #ifndef _GRID_WORLD_CONVERTER_H_
 #define _GRID_WORLD_CONVERTER_H_
 
-#include <sbpl/utils/utils.h>
+//#include <sbpl/utils/utils.h>
 #include <Eigen/Core>
 
 class GridConverter{
 public:
     GridConverter(int numThetaDirs);
-    
+
+    int numThetaDirs() const;
+    double mapOffsetX() const;
+    double mapOffsetY() const;
+    void mapOffset(double& x, double& y) const;
+    double cellSize() const;
+
+    void setNumThetaDirs(const int n);
+    void setMapOffset(const double xOffset, const double yOffset);
+    void setCellSize(const double cellSize);
+
+    int contXYToDisc(const double x) const;
+    double discXYToCont(const int x) const;
+
+    // converts discretized version of angle into continuous (radians)
+    // maps 0->0, 1->delta, 2->2*delta, ...
+    double discThetaToCont(int nTheta) const;
+
+    // converts continuous (radians) version of angle into discrete
+    //maps 0->0, [delta/2, 3/2*delta)->1, [3/2*delta, 5/2*delta)->2,...
+    int contThetaToDisc(double fTheta) const;
+
     // functions to convert from grid indices to world coordinates (cell center)
-//      sbpl_xy_theta_pt_t gridToWorld(const int x, const int y, const int theta) const;
     void gridToWorld(const int x_d, const int y_d, const int theta_d, double& x_c, double& y_c, double& theta_c) const;
     void grid2dToWorld(const int x_d, const int y_d, double& x_c, double& y_c) const;
     void grid2dToWorld(const Eigen::Vector2i gridCoords, Eigen::Vector2d& worldCoords) const;
     // functions to convert from world coordinates to grid indices
-    //void worldToGrid(const sbpl_xy_theta_pt_t& pose, int& x_d, int& y_d, int& theta_d) const;
     void worldToGrid(const double x_c, const double y_c, const double theta_c, int& x_d, int& y_d, int& theta_d) const;
     void world2dToGrid(const double x_c, const double y_c, int& x_d, int& y_d) const;
     Eigen::Vector2i world2dToGrid(const Eigen::Vector2d& xy_c) const;
     // convert from continuous to gridmap coordinates, do not apply offset
     void cont2dToGrid(const double x_c, const double y_c, int& x_d, int& y_d) const;
     void cont2dToGrid(const Eigen::Vector2d xy_c, Eigen::Vector2i& xy_d) const;
-    //void contToGrid(const sbpl_xy_theta_pt_t& pose, int& x_d, int& y_d, int& theta_d) const;
     // convert from gridmap to continuous coordinates, do not apply offset
     void grid2dToCont(const int x_d, const int y_d, double& x_c, double& y_c) const;
     void grid2dToCont(const Eigen::Vector2i xy_d, Eigen::Vector2d& xy_c) const;
-    //void gridToCont(const int x_d, const int y_d, const int theta_d, sbpl_xy_theta_pt_t& pose) const;
 
-    int numThetaDirs; // the number of discrete theta directions
-    double mapOffsetX, mapOffsetY; // the grid offset in world coordinates
-    double cellSize; // the size of one cell in world coordinates
+protected:
+    int numThetaDirs_; // the number of discrete theta directions
+    double thetaBinSize_;
+    double mapOffsetX_, mapOffsetY_; // the grid offset in world coordinates
+    double cellSize_; // the size of one cell in world coordinates
 };
 
 #endif // _GRID_WORLD_CONVERTER_H_
--- src/grid_world_conversion.cpp
+++ src/grid_world_conversion.cpp
@@ -1,114 +1,184 @@
-#include "bonirob_traversability_checker/grid_converter.h"
+#include "grid_world_conversion/grid_world_conversion.h"
 
+#include <angles/angles.h>
+#include <iostream>
 //#include <sbpl/utils/utils.h>
 #include <Eigen/Core>
 
-namespace Flourish{
-  namespace BoniRob{
-    GridConverter::GridConverter(int numThetaDirs):
-      numThetaDirs(numThetaDirs),
-      mapOffsetX(0),
-      mapOffsetY(0),
-      cellSize(1)
-    {
+GridConverter::GridConverter(int numThetaDirs):
+    numThetaDirs_(numThetaDirs),
+    thetaBinSize_(2.0 * M_PI / numThetaDirs_),
+    mapOffsetX_(0),
+    mapOffsetY_(0),
+    cellSize_(1)
+{
     
+}
+
+int GridConverter::numThetaDirs() const
+{
+    return numThetaDirs_;
+}
+
+double GridConverter::mapOffsetX() const
+{
+    return mapOffsetX_;
+}
+
+double GridConverter::mapOffsetY() const
+{
+    return mapOffsetY_;
+}
+
+void GridConverter::mapOffset(double& x, double& y) const
+{
+    x = mapOffsetX_;
+    y = mapOffsetY_;
+}
+
+double GridConverter::cellSize() const
+{
+    return cellSize_;
+}
+
+void GridConverter::setNumThetaDirs(const int n)
+{
+    numThetaDirs_ = n;
+    thetaBinSize_ = 2.0 * M_PI / numThetaDirs_;
+}
+
+void GridConverter::setMapOffset(const double xOffset, const double yOffset)
+{
+    mapOffsetX_ = xOffset;
+    mapOffsetY_ = yOffset;
+}
+
+void GridConverter::setCellSize(const double cellSize)
+{
+    cellSize_ = cellSize;
+}
+
+int GridConverter::contXYToDisc(const double x) const
+{
+    if(x >= 0){
+        return x/cellSize_;
+    }else{
+        return x/cellSize_-1;
     }
+}
+
+double GridConverter::discXYToCont(const int x) const
+{
+    return x*cellSize_ + cellSize_/2.;
+}
+
+double GridConverter::discThetaToCont(const int nTheta) const
+{
+    return nTheta * thetaBinSize_;
+}
+
+int GridConverter::contThetaToDisc(const double fTheta) const
+{
+    return (int)(angles::normalize_angle_positive(fTheta + thetaBinSize_ / 2.0) / (2.0 * M_PI) * (numThetaDirs_));
+}
+
 
 /*    sbpl_xy_theta_pt_t GridConverter::gridToWorld(const int x_d, const int y_d, const int theta_d) const
-    {
+      {
       sbpl_xy_theta_pt_t pose;
-      pose.x = DISCXY2CONT(x_d, cellSize)+mapOffsetX;
-      pose.y = DISCXY2CONT(y_d, cellSize)+mapOffsetY;
-      pose.theta = DiscTheta2Cont(theta_d, numThetaDirs);
+      pose.x = discXYToCont(x_d)+mapOffsetX;
+      pose.y = discXYToCont(y_d)+mapOffsetY_;
+      pose.theta = discThetaToCont(theta_d);
       return pose;
       }*/
 
-    void GridConverter::gridToWorld(const int x_d, const int y_d, const int theta_d, double& x_c, double& y_c, double& theta_c) const
-    {
-      x_c = DISCXY2CONT(x_d, cellSize)+mapOffsetX;
-      y_c = DISCXY2CONT(y_d, cellSize)+mapOffsetY;
-      theta_c = DiscTheta2Cont(theta_d, numThetaDirs);
-    }
+void GridConverter::gridToWorld(const int x_d, const int y_d, const int theta_d, double& x_c, double& y_c, double& theta_c) const
+{
+    x_c = discXYToCont(x_d)+mapOffsetX_;
+    y_c = discXYToCont(y_d)+mapOffsetY_;
+    theta_c = discThetaToCont(theta_d);
+}
 
-    void GridConverter::grid2dToWorld(const int x_d, const int y_d, double& x_c, double& y_c) const
-    {
-      x_c = DISCXY2CONT(x_d, cellSize)+mapOffsetX;
-      y_c = DISCXY2CONT(y_d, cellSize)+mapOffsetY;
-    }
+void GridConverter::grid2dToWorld(const int x_d, const int y_d, double& x_c, double& y_c) const
+{
+    x_c = discXYToCont(x_d)+mapOffsetX_;
+    y_c = discXYToCont(y_d)+mapOffsetY_;
+}
 
-    void GridConverter::grid2dToWorld(const Eigen::Vector2i gridCoords, Eigen::Vector2d& worldCoords) const
-    {
-      worldCoords.x() = DISCXY2CONT(gridCoords.x(), cellSize)+mapOffsetX;
-      worldCoords.y() = DISCXY2CONT(gridCoords.y(), cellSize)+mapOffsetY;
-    }
+void GridConverter::grid2dToWorld(const Eigen::Vector2i gridCoords, Eigen::Vector2d& worldCoords) const
+{
+    worldCoords.x() = discXYToCont(gridCoords.x())+mapOffsetX_;
+    worldCoords.y() = discXYToCont(gridCoords.y())+mapOffsetY_;
+}
 
 /*    void GridConverter::worldToGrid(const sbpl_xy_theta_pt_t& pose, int& x_d, int& y_d, int& theta_d) const
-    {
-      x_d = CONTXY2DISC(pose.x - mapOffsetX, cellSize);
-      y_d = CONTXY2DISC(pose.y - mapOffsetY, cellSize);
-      theta_d = ContTheta2Disc(pose.theta, numThetaDirs);
-      theta_d = NORMALIZEDISCTHETA(theta_d, numThetaDirs);
+      {
+      x_d = contXYToDisc(pose.x - mapOffsetX_);
+      y_d = contXYToDisc(pose.y - mapOffsetY_);
+      theta_d = contThetaToDisc(pose.theta);
+      theta_d = NORMALIZEDISCTHETA(theta_d, numThetaDirs_);
       }*/
 
-    void GridConverter::worldToGrid(const double x_c, const double y_c, const double theta_c, int& x_d, int& y_d, int& theta_d) const
-    {
-      x_d = CONTXY2DISC(x_c - mapOffsetX, cellSize);
-      y_d = CONTXY2DISC(y_c - mapOffsetY, cellSize);
-      theta_d = ContTheta2Disc(theta_c, numThetaDirs);
-      theta_d = NORMALIZEDISCTHETA(theta_d, numThetaDirs);
+void GridConverter::worldToGrid(const double x_c, const double y_c, const double theta_c, int& x_d, int& y_d, int& theta_d) const
+{
+    x_d = contXYToDisc(x_c - mapOffsetX_);
+    y_d = contXYToDisc(y_c - mapOffsetY_);
+    theta_d = contThetaToDisc(theta_c);
+    //theta_d = NORMALIZEDISCTHETA(theta_d);
+    if(theta_d < 0 || theta_d >= numThetaDirs_){
+        std::cerr << "ERROR: got discrete theta smaller than 0 or larger than number of valid theta values!" << std::endl;
     }
+}
 
-    void GridConverter::world2dToGrid(const double x_c, const double y_c, int& x_d, int& y_d) const
-    {
-      x_d = CONTXY2DISC(x_c - mapOffsetX, cellSize);
-      y_d = CONTXY2DISC(y_c - mapOffsetY, cellSize);
-    }
+void GridConverter::world2dToGrid(const double x_c, const double y_c, int& x_d, int& y_d) const
+{
+    x_d = contXYToDisc(x_c - mapOffsetX_);
+    y_d = contXYToDisc(y_c - mapOffsetY_);
+}
 
-    Eigen::Vector2i GridConverter::world2dToGrid(const Eigen::Vector2d& xy_c) const
-    {
-      Eigen::Vector2i xy_d;
-      xy_d.x() = CONTXY2DISC(xy_c.x() - mapOffsetX, cellSize);
-      xy_d.y() = CONTXY2DISC(xy_c.y() - mapOffsetY, cellSize);
-      return xy_d;
-    }
+Eigen::Vector2i GridConverter::world2dToGrid(const Eigen::Vector2d& xy_c) const
+{
+    Eigen::Vector2i xy_d;
+    xy_d.x() = contXYToDisc(xy_c.x() - mapOffsetX_);
+    xy_d.y() = contXYToDisc(xy_c.y() - mapOffsetY_);
+    return xy_d;
+}
 
-    void GridConverter::cont2dToGrid(const double x_c, const double y_c, int& x_d, int& y_d) const
-    {
-      x_d = CONTXY2DISC(x_c, cellSize);
-      y_d = CONTXY2DISC(y_c, cellSize);
-    }
+void GridConverter::cont2dToGrid(const double x_c, const double y_c, int& x_d, int& y_d) const
+{
+    x_d = contXYToDisc(x_c);
+    y_d = contXYToDisc(y_c);
+}
 
-    void GridConverter::cont2dToGrid(const Eigen::Vector2d xy_c, Eigen::Vector2i& xy_d) const
-    {
-      xy_d.x() = CONTXY2DISC(xy_c.x(), cellSize);
-      xy_d.y() = CONTXY2DISC(xy_c.y(), cellSize);
-    }
+void GridConverter::cont2dToGrid(const Eigen::Vector2d xy_c, Eigen::Vector2i& xy_d) const
+{
+    xy_d.x() = contXYToDisc(xy_c.x());
+    xy_d.y() = contXYToDisc(xy_c.y());
+}
 
-  /*void GridConverter::contToGrid(const sbpl_xy_theta_pt_t& pose, int& x_d, int& y_d, int& theta_d) const
-    {
-      x_d = CONTXY2DISC(pose.x, cellSize);
-      y_d = CONTXY2DISC(pose.y, cellSize);
-      theta_d = ContTheta2Disc(pose.theta, numThetaDirs);
-      }*/
+/*void GridConverter::contToGrid(const sbpl_xy_theta_pt_t& pose, int& x_d, int& y_d, int& theta_d) const
+  {
+  x_d = contXYToDisc(pose.x);
+  y_d = contXYToDisc(pose.y);
+  theta_d = contThetaToDisc(pose.theta);
+  }*/
 
-    void GridConverter::grid2dToCont(const int x_d, const int y_d, double& x_c, double& y_c) const
-    {
-      x_c = DISCXY2CONT(x_d, cellSize);
-      y_c = DISCXY2CONT(y_d, cellSize);
-    }
+void GridConverter::grid2dToCont(const int x_d, const int y_d, double& x_c, double& y_c) const
+{
+    x_c = discXYToCont(x_d);
+    y_c = discXYToCont(y_d);
+}
 
-    void GridConverter::grid2dToCont(const Eigen::Vector2i xy_d, Eigen::Vector2d& xy_c) const
-    {
-      xy_c.x() = DISCXY2CONT(xy_d.x(), cellSize);
-      xy_c.y() = DISCXY2CONT(xy_d.y(), cellSize);
-    }
+void GridConverter::grid2dToCont(const Eigen::Vector2i xy_d, Eigen::Vector2d& xy_c) const
+{
+    xy_c.x() = discXYToCont(xy_d.x());
+    xy_c.y() = discXYToCont(xy_d.y());
+}
 
 /*    void GridConverter::gridToCont(const int x_d, const int y_d, const int theta_d, sbpl_xy_theta_pt_t& pose) const
-    {
-      pose.x = DISCXY2CONT(x_d, cellSize);
-      pose.y = DISCXY2CONT(y_d, cellSize);
-      pose.theta = DiscTheta2Cont(theta_d, numThetaDirs);
-    }
+      {
+      pose.x = discXYToCont(x_d);
+      pose.y = discXYToCont(y_d);
+      pose.theta = discThetaToCont(theta_d);
+      }
 */
-  };
-};
